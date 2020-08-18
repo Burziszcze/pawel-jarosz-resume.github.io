cf21e204139f9ce97d90515ed2c8b25514c6eda2
burziszcze <burziszcze@gmail.com>
2020-08-18 18:50:08 +0200

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+"use strict";
+
+// Load plugins
+const autoprefixer = require("autoprefixer");
+const cssnano = require("cssnano");
+const gulp = require("gulp");
+const plumber = require("gulp-plumber");
+const postcss = require("gulp-postcss");
+const rename = require("gulp-rename");
+const sass = require("gulp-sass");
+
+// CSS task
+function css() {
+    return gulp
+        .src("./assets/scss/**/*.scss")
+        .pipe(plumber())
+        .pipe(sass({ outputStyle: "expanded" }))
+        .pipe(gulp.dest("./assets/css/"))
+        .pipe(rename({ suffix: ".min" }))
+        .pipe(postcss([autoprefixer(), cssnano()]))
+        .pipe(gulp.dest("./assets/css/"))
+}
+
+// Watch files
+function watchFiles() {
+    gulp.watch("./assets/scss/**/*", css);
+}
+
+// define complex tasks
+const build = gulp.series(gulp.parallel(css));
+const watch = gulp.parallel(watchFiles);
+
+// export tasks
+exports.watch = watch;
+exports.build = build;
+exports.default = watch;
